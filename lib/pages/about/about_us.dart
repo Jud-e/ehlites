e3138e5dac8ca271d8f54570efcd506bc893a757
Jude Enuanwa <lenuanwa@gmail.com>
2023-07-05 09:20:27 +0100

    update

--- lib/pages/about/about_us.dart
+++ lib/pages/about/about_us.dart
@@ -1,7 +1,10 @@
+import 'package:ehlites/utilities/constants.dart';
+import 'package:ehlites/utilities/text_style_util.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
+import '../../utilities/colors.dart';
 import '../../utilities/drawer.dart';
 import '../../utilities/navbar_content.dart';
 
@@ -22,6 +25,43 @@ class _AboutState extends State<About> {
           child: screenSize.width < 760
               ? const CustomDrawer()
               : const TopNavContent()),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 600,
+              width: screenSize.width,
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: screenSize.width * 0.124,
+                    vertical: screenSize.height * 0.124),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Column(
+                          children: [
+                            Text(
+                              "About Us",
+                              style: normalStyle(25, secondary),
+                            ),
+                            yMargin(5.0),
+                            Text(
+                              "We are a non-profit organization",
+                              style: thickStyle(37, black),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
