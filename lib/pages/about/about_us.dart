ac53a89280d822360c58fb25ddb60b98de8812d0
Jude Enuanwa <lenuanwa@gmail.com>
2023-07-05 11:27:58 +0100

    editing

--- lib/pages/about/about_us.dart
+++ lib/pages/about/about_us.dart
@@ -26,38 +26,80 @@ class _AboutState extends State<About> {
               ? const CustomDrawer()
               : const TopNavContent()),
       body: SingleChildScrollView(
+        // scrollDirection: Axis.vertical,
         child: Column(
           children: [
-            Container(
+            SizedBox(
               height: 600,
               width: screenSize.width,
-              child: Padding(
-                padding: EdgeInsets.symmetric(
-                    horizontal: screenSize.width * 0.124,
-                    vertical: screenSize.height * 0.124),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: screenSize.width * 0.124,
+                          vertical: screenSize.height * 0.124),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "About Us",
+                                style: normalStyle(30, secondary),
+                              ),
+                              yMargin(5.0),
+                              Text(
+                                "We are a non-profit organization\n focused on celebrating and\n empowering young girls and women\n in technology across africa",
+                                style: thickStyle(20, black),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            child: Padding(
+                              padding: EdgeInsets.all(20),
+                              child: Text("Insert image"),
+                            ),
+                          )
+                        ],
+                      )),
+                ],
+              ),
+            ),
+            Container(
+                height: 500,
+                width: double.infinity,
+                decoration: BoxDecoration(color: purple),
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Row(
-                      children: [
-                        Column(
-                          children: [
-                            Text(
-                              "About Us",
-                              style: normalStyle(25, secondary),
-                            ),
-                            yMargin(5.0),
-                            Text(
-                              "We are a non-profit organization",
-                              style: thickStyle(37, black),
-                            ),
-                          ],
-                        ),
-                      ],
+                    Text(
+                      "WHAT WE DO",
+                      style: thickStyle(20, primary,
+                          decoration: TextDecoration.underline),
+                    ),
+                    yMargin(20),
+                    Text(
+                      "WE PUT OUR MONEY\n WHERE OUR MOUTH IS",
+                      style: heavyStyle(25, primary),
+                    ),
+                    yMargin(20),
+                    Text(
+                      "Forget top-down philantrophy. We get core, flexible funding and\n resources to feminist activists to drive gender equality around the world.",
+                      style: heavyStyle(25, primary),
                     ),
                   ],
-                ),
-              ),
+                )),
+            SizedBox(
+              width: double.infinity,
+              height: 200,
+              child: Center(
+                  child: Text(
+                "Core Values",
+                style: thickStyle(50, black),
+              )),
             ),
           ],
         ),
