28f83964fc1a75b3fcebe8d46ca19348ed7aefde
Jude Enuanwa <lenuanwa@gmail.com>
2023-07-17 19:29:11 +0100

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: About(),
+      home: HomePage(),
       debugShowCheckedModeBanner: false,
       // routes: {'/about us': (context) => const About()},
     );
