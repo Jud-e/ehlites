cb0375a7fa9577e0fa1f7a56489cc7c8bc907c39
Jude Enuanwa <lenuanwa@gmail.com>
2023-07-18 14:29:11 +0100

    updates

--- lib/pages/about/team.dart
+++ lib/pages/about/team.dart
@@ -22,26 +22,34 @@ class Team extends StatelessWidget {
               child: Row(
                 children: [
                   SizedBox(
-                    height: 200,
-                    width: 200,
+                    height: 250,
+                    width: 250,
                     child: Card(
-                      color: secondary,
-                      child: Center(
-                          child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.abc,
-                            color: green,
-                            size: 100,
-                          ),
-                          yMargin(10),
-                          Text(
-                            "data",
-                            style: heavyStyle(30, green),
-                          )
-                        ],
-                      )),
+                      color: purple,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Center(
+                            child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset(
+                              "images/pic1.jpg",
+                              fit: BoxFit.fill,
+                              width: 250,
+                              height: 180,
+                            ),
+                            // yMargin(10),
+                            Text(
+                              "data",
+                              style: normalStyle(18, primary),
+                            ),
+                            Text(
+                              "data",
+                              style: normalStyle(10, primary),
+                            )
+                          ],
+                        )),
+                      ),
                     ),
                   ),
                   xMargin(80),
